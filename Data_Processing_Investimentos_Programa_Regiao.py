4df6fe3aa5660dcf1ddb91eef62e8da8e70392a9
Paulo Icaro <87305764+paulo-icaro@users.noreply.github.com>
2025-09-29 15:55:53 -0300

    Versão estável para correção de dados cumulativos

--- Data_Processing_Investimentos_Programa_Regiao.py
+++ Data_Processing_Investimentos_Programa_Regiao.py
@@ -131,22 +131,31 @@ for x in range(len(folder_files)):
 # === Adjustments for cumulative data === #
 # ======================================= #
 
-# --- Subseting --- #
+# --- Subseting - Removing 2012 data --- #
 dataset_full = dataset_full[dataset_full.ano != '2012']
 
 # --- Sorting --- #
-dataset_full = dataset_full.sort_values(by = ['regiao', 'cod_program', 'tipo', 'ano', 'mes'])
+if info_desired == 'p':
+    dataset_full = dataset_full.sort_values(by = ['cod_program', 'tipo', 'ano', 'mes'])
+else:
+    dataset_full = dataset_full.sort_values(by = ['regiao', 'cod_program', 'tipo', 'ano', 'mes'])
 
 # --- Cumulative data adjustment --- #
-dataset_full = dataset_full.assign(empenhado_ajustado = '', pago_ajustado = '')
-dataset_full['empenhado_ajustado'] = dataset_full['empenhado'] - dataset_full['empenhado'].shift(1)
+dataset_full = dataset_full.assign(empenhado_ajustado = '', pago_ajustado = '')                         # Inserting columns
+dataset_full['empenhado_ajustado'] = dataset_full['empenhado'] - dataset_full['empenhado'].shift(1)     # Inserting adjusted values to the column
+dataset_full['pago_ajustado'] = dataset_full['pago'] - dataset_full['pago'].shift(1)                    # Inserting adjusted values to the column
 
+# --- Loop for adjusting january values --- #
 for i in range(len(dataset_full)):
     if dataset_full['mes'].iloc[i] == "01":
         dataset_full['empenhado_ajustado'].iloc[i] = dataset_full['empenhado'].iloc[i]
-    '''else:
-        dataset_full['empenhado_ajustado'].iloc[i] = dataset_full['empenhado'].iloc[i] - dataset_full['empenhado'].iloc[i-1]'''
+        dataset_full['pago_ajustado'].iloc[i] = dataset_full['pago'].iloc[i]
+    
+# --- Subseting - Removing cumulative columns --- #
+dataset_full = dataset_full.drop(columns = ['empenhado', 'pago'])
 
+# --- Renaming columns --- #
+dataset_full.rename(columns = {'empenhado_ajustado':'empenhado', 'pago_ajustado':'pago'}, inplace = True)
 
 
 
@@ -161,7 +170,7 @@ if info_desired == 'p':
     # Vertical dataset adjustment
     dataset_full = dataset_full.melt(
         id_vars = ['periodo', 'ano', 'mes', 'tipo', 'cod_program', 'program'],
-        value_vars = ['lei', 'lei+cred', 'empenhado', 'pago', '%emp', '%pago'],
+        value_vars = ['empenhado', 'pago'],
         var_name = 'categoria',
         value_name = 'valor'
         )
@@ -170,23 +179,23 @@ if info_desired == 'p':
         dataset_full.to_excel(excel_writer = writer, sheet_name = 'investimentos_programa', index = False)
 
         # Just Formatting the Excel Sheet (not needed in case of vertical adjustment)
-        #workbook = writer.book
-        #worksheet = writer.sheets['investimentos_programa']
-        #money_formatting = workbook.add_format({'num_format':'R$#,##0'})
-        #perc_formatting = workbook.add_format({'num_format':'0.0%'})
-        #worksheet.set_column('G:J', 15, money_formatting)
-        #worksheet.set_column('K:L', 15, perc_formatting)
-        #worksheet.set_column('A:F', 15)
+        '''workbook = writer.book
+        worksheet = writer.sheets['investimentos_programa']
+        money_formatting = workbook.add_format({'num_format':'R$#,##0'})
+        perc_formatting = workbook.add_format({'num_format':'0.0%'})
+        worksheet.set_column('G:J', 15, money_formatting)
+        worksheet.set_column('K:L', 15, perc_formatting)
+        worksheet.set_column('A:F', 15)'''
     
     # Full Cleasing
-    del(dataset, folder_files, i, info_desired, n_char, remove_rows, writer, x)#, money_formatting, perc_formatting, workbook, worksheet)
+    del(dataset, folder_files, i, info_desired, n_char, remove_rows, writer, x, new, old, replacements)#, money_formatting, perc_formatting, workbook, worksheet)
 
 else:
     
     # Vertical dataset adjustment
     dataset_full = dataset_full.melt(
         id_vars = ['periodo', 'ano', 'mes', 'tipo', 'cod_regiao', 'regiao', 'cod_program', 'program'],
-        value_vars = ['lei', 'lei+cred', 'empenhado', 'pago', '%emp', '%pago'],
+        value_vars = ['empenhado', 'pago'],
         var_name = 'categoria',
         value_name = 'valor'
         )
@@ -206,7 +215,7 @@ else:
         worksheet.set_column('A:F', 15)'''
     
     # Full Cleasing
-    del(dataset, folder_files, i, info_desired, n_char, remove_rows, writer, x, last_program, cod_last_program)#, money_formatting, perc_formatting, workbook, worksheet)
+    del(dataset, folder_files, i, info_desired, n_char, remove_rows, writer, x, last_program, cod_last_program, new, old, replacements)#, money_formatting, perc_formatting, workbook, worksheet)
 
         
 
